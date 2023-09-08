5fbccf9d4e616c07748c7d9110c078f463e04d1c
julia <juliaionova111@gmail.com>
2023-09-08 17:01:02 +0300

    Add id to UserDTO

--- FitnessBooking.BLL/DTOs/Users/Responses/UserDTO.cs
+++ FitnessBooking.BLL/DTOs/Users/Responses/UserDTO.cs
@@ -2,6 +2,7 @@ namespace FitnessBooking.BLL.DTOs.Users.Responses;
 
 public class UserDTO
 {
+    public string Id { get; set; } = null!;
     public string Username { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
